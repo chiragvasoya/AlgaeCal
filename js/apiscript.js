f15c5be8797ff39b68aac9bbe0c203ff80166773
Chirag Vasoya <vasoyachirag@live.com>
2017-05-14 12:39:11 -0600

    7year  data and footer button link update commit

--- js/apiscript.js
+++ js/apiscript.js
@@ -1,10 +1,10 @@
 var apihttp = new XMLHttpRequest();
 apihttp.onreadystatechange = function(){
-   
+    
     if(this.readyState == 4 && this.status == 200) {
         apiObj = JSON.parse(this.responseText);
         document.getElementById("ph-number").innerHTML = apiObj.acf.default_phone_number;
-        document.getElementById("guarantee-info").innerHTML = apiObj.acf.7yr_full_copy;
+        document.getElementById("guarantee-info").innerHTML = apiObj.acf["7yr_full_copy"];
     }
     
 };
